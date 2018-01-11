d6164c3213df6f4b173ed5ea0cdf83dd4f44c50b
gvillablanca <g.villalobo.m@gmail.com>
2018-01-11 14:02:16 -0300

    unificado de codigo

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,39 +3,8 @@
   * funcion de carrusel en seccion de pelicula
   * botones laterales de seccion pelicula
   */
- 	$(".img").hide();
-  $(".img").first().show();  
-   
+
  });
-
-
-
- /*
-   var images = [];
-  $('.img').each(function(){
-       images.push($(this));    
-   });
-
-   var imgcount = images.length;
-   $(".img").hide();
-   $(".img").first().show();
-   var currentItem = 0;
-
-   $('.right').click(function(){
-           $("img").hide();
-           currentItem++;
-           images[currentItem%imgcount].show();        
-   });
-
-
-   $('.left').click(function(){
-           $("img").hide();
-          currentItem--;
-          if(currentItem < 0)
-              currentItem = imgcount-1;
-          images[currentItem%imgcount].show();
-   });
- */
 
 $(document).ready(function(){
 	$("#film_logo").click(function(){
@@ -46,3 +15,4 @@ $(document).ready(function(){
         interval: 3000
     })
 });
+
