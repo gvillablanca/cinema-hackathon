c87db06d9c8a2b39f10c622028e4e0da17bb5645
gvillablanca <g.villalobo.m@gmail.com>
2018-01-12 13:48:23 -0300

    funcionalidad de busqueda

--- assets/js/app.js
+++ assets/js/app.js
@@ -119,3 +119,95 @@ $(function () {
         $(".login").show();
     }, 2000);
 });
+
+
+$(document).ready(() => {
+  $('#searchForm').on('submit', (e) => {
+    let searchText = $('#searchText').val();
+    getMovies(searchText);
+    e.preventDefault();
+  });
+});
+
+$(document).ready(() => {
+   $('#searchForm').on('submit', (e) => {
+      let searchText = $('#searchText').val();
+       getMovies(searchText);
+      e.preventDefault(); 
+   });
+                  
+});
+
+function getMovies(searchText){
+    axios.get('https://api.themoviedb.org/3/search/movie?api_key=fa155f635119344d33fcb84fb807649b&query='+searchText)
+        .then((response) => {
+            console.log(response);
+            let movies = response.data.results;
+            let output = '';
+            $.each(movies, (index, movie) =>{
+                output += `
+                    <div class="col-md-3">
+                        <div class="well text-center">
+                            <img src="http://image.tmdb.org/t/p/w185/${movie.poster_path}">
+                            <h5>${movie.title}</h5>
+                            <a onclick="movieSelected('${movie.id}')" class="btn btn-primary" href="#">Movie Details</a>
+                        </div>
+                    </div>
+            `;
+            });
+        
+        $('#movies').html(output);
+        })
+        .catch((err) =>{
+           console.log(err);
+        });
+}﻿
+function movieSelected(id){
+  sessionStorage.setItem('movieId', id);
+  window.location = 'movie.html';
+  return false;
+}
+
+function getMovie(){
+  let movieId = sessionStorage.getItem('movieId');
+  axios.get('http://www.omdbapi.com?i='+movieId)
+    .then((response) => {
+      console.log(response);
+      let movie = response.data;
+
+      let output =`
+        <div class="row">
+          <div class="col-md-4">
+            <img src="${movie.Poster}" class="thumbnail">
+          </div>
+          <div class="col-md-8">
+            <h2>${movie.Title}</h2>
+            <ul class="list-group">
+              <li class="list-group-item"><strong>Genre:</strong> ${movie.Genre}</li>
+              <li class="list-group-item"><strong>Released:</strong> ${movie.Released}</li>
+              <li class="list-group-item"><strong>Rated:</strong> ${movie.Rated}</li>
+              <li class="list-group-item"><strong>IMDB Rating:</strong> ${movie.imdbRating}</li>
+              <li class="list-group-item"><strong>Director:</strong> ${movie.Director}</li>
+              <li class="list-group-item"><strong>Writer:</strong> ${movie.Writer}</li>
+              <li class="list-group-item"><strong>Actors:</strong> ${movie.Actors}</li>
+            </ul>
+          </div>
+        </div>
+        <div class="row">
+          <div class="well">
+            <h3>Plot</h3>
+            ${movie.Plot}
+            <hr>
+            <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
+            <a href="index.html" class="btn btn-default">Go Back To Search</a>
+          </div>
+        </div>
+      `;
+      $(".default-movies").hide();
+
+      $('#movie').html(output);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
